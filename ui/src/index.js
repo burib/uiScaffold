072103652cd609f1f946a195fa3454777debc0d3
Balázs Buri <info@burib.net>
2015-11-03 11:49:40 +0100

    fix jscs problem.

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,7 +1,7 @@
 require('./index.scss'); // global css variables and rules for the whole app
 
 angular.module('app', [])
-  .config(function($routeProvider, $locationProvider     ) {
+  .config(function($routeProvider, $locationProvider) {
     // This sets the url mode to html5 clean URLs.
     $locationProvider.html5Mode(true);
   });
